acf076f31e1af413c880b8d056e99569086d2e3b
MuchinaWamburia <lucymuchina20@gmail.com>
2023-01-26 15:42:12 +0300

    modified

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React  from 'react';
+import  './App.css';
+import CardContainer from './Components/CardContainer';
+
+function App(){
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <div className="App-body">
+    < CardContainer  />
+      </div>
+    </div>
+  );
+}
+
+export default App;
