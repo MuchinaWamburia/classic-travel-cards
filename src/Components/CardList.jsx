3a3ebd18491feafd95bb6fdb69dd7b8a93efb8f1
MuchinaWamburia <lucymuchina20@gmail.com>
2023-01-27 22:57:33 +0300

    Updated cards

--- src/Components/CardList.jsx
+++ src/Components/CardList.jsx
@@ -1,15 +1,32 @@
 import React from "react";
+import "./style.css"
+import CardLayout from "./CardLayout"
 
-function CardList ( props) {
+//   props) {
 
-  return (
-    <div className="cardlist">
-      <img src={props.imgUrl} alt={props.title} title={props.title} />
-       <div className='card-title-container'>
-       <span className='card-title'>{props.title.toUpperCase()}</span>
-    </div>
-    </div>
-  )
+//   return (
+//     <div className="cardlist">
+//       <img src={props.imgUrl} alt={props.title} title={props.title} />
+//        <div className='card-title-container'>
+//        <span className='card-title'>{props.title.toUpperCase()}</span>
+//     </div>
+//     </div>
+//   )
+// }
+function CardList ( {searchResults}){
+
+  const cardList =searchResults.map (card => {
+    return < CardLayout  description={card.description} 
+    image={card.image} 
+    location={card.location}
+    card={card} 
+    key={card.id}/>
+  })
+   return (
+  <div className="cardlayout">
+      {cardList}
+  </div>
+ )
 }
 
 export default CardList;
